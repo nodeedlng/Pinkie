7041f7efdeb10067324fa9f855b05f83ff871b87
Bryan Yick <ycby@st-andrews.ac.uk>
2019-02-03 22:25:27 +0000

    initial prototype design 1, begin second design

--- pinkieSite/scripts/navbar.js
+++ pinkieSite/scripts/navbar.js
@@ -7,9 +7,31 @@ var nav_bar = document.getElementsByTagName("nav");
 var sections = nav_bar[0].getElementsByTagName("li");
 
 for (var i = 0; i < sections.length; i ++){
+    
     sections[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
+        
+        var current = getCurrent();
+    
+        //Hide if section not active
+        hide(current[0].id + "content");
+        
         current[0].className = current[0].className.replace("active", "");
         this.className += "active";
+        
+        //Show if section active
+        show(current[0].id + "content");
     });
+}
+
+
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+function show(id) {
+    document.getElementById(id).style.display = "inherit";
+}
+
+function getCurrent() {
+    return document.getElementsByClassName("active");
 }
---
+++ pinkieSite/scripts/sections.js
@@ -0,0 +1,7 @@
+/* Only show the sections necessary */
+
+var postcontainers = document.getElementsByClassName("postcontainer");
+
+for (var i = 0; i < postcontainers.length; i++) {
+    postcontainers[i].style.display = "none";
+}
