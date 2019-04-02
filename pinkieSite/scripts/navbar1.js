bee84af0e2be6f727a04e559ad6eff265a2876c2
Bryan Yick <ycby@st-andrews.ac.uk>
2019-04-02 13:56:39 +0100

    Started filling with content

--- pinkieSite/scripts/navbar1.js
+++ pinkieSite/scripts/navbar1.js
@@ -1,3 +1,5 @@
+/*eslint-env browser*/
+
 /* For changing the active element in the nav bar */
 
 /* Adapted from example on https://www.w3schools.com/howto/howto_js_active_element.asp */
@@ -6,15 +8,50 @@ var nav_bar = document.getElementsByTagName("nav");
 
 var sections = nav_bar[0].getElementsByTagName("li");
 
-for (var i = 0; i < sections.length; i ++){
+for (var j = 0; j < sections.length; j ++){
     
-    sections[i].addEventListener("click", function() {
+    sections[j].addEventListener("click", function() {
         
         var current = getCurrent();
+    
+        //Hide if section not active
+        hide(current[0].id + "container");
         
         current[0].className = current[0].className.replace("active", "");
         this.className += "active";
+        
+        //Show if section active
+        show(current[0].id + "container");
     });
+}
+
+
+var unitnav = document.getElementById("unitnav");
+
+var unitsections = unitnav.getElementsByTagName("li");
+
+for (var i = 0; i < unitsections.length; i ++) {
+    
+    unitsections[i].addEventListener("click", function() {
+        
+        var currentunit = document.getElementsByClassName("activeunit");
+        
+        document.getElementById(currentunit[0].id.substring(0, currentunit[0].id.length - 3)).style.display = "none";
+        
+        currentunit[0].className = currentunit[0].className.replace("activeunit", "");
+        this.className += "activeunit";
+        
+        document.getElementById(currentunit[0].id.substring(0, currentunit[0].id.length - 3)).style.display = "inherit";
+       
+    });
+}
+
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+function show(id) {
+    document.getElementById(id).style.display = "inherit";
 }
 
 function getCurrent() {
