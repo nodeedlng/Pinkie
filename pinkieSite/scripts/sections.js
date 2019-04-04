3a205c894848d7ad03993088ee3c7d7906f3815b
Bryan Yick <ycby@st-andrews.ac.uk>
2019-04-04 14:48:32 +0100

    restructured some files so that the files can be pushed to website easily

--- pinkieSite/scripts/sections.js
+++
@@ -1,7 +0,0 @@
-/* Only show the sections necessary */
-
-var postcontainers = document.getElementsByClassName("postcontainer");
-
-for (var i = 0; i < postcontainers.length; i++) {
-    postcontainers[i].style.display = "none";
-}
